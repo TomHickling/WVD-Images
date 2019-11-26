67219c00061db716e22c4d1d71fc7a930f21d44b
Tom Hickling <tomhickling@hotmail.com>
2019-11-26 10:50:49 +0000

    Add files via upload

--- 1.AzureImageBuilder/CreateAIBImage.ps1
+++ 1.AzureImageBuilder/CreateAIBImage.ps1
@@ -1,26 +1,26 @@
 ﻿##Create a VM image using Azure Image Builder that we will later store in Shared IMage Gallery and deploy to a WVD Host Pool
 
 #Set Variables - Resource Group to deploy into and the ARM template we use later
-$RG = "WVD_EUS_AzureImageBuilder2"
+$RG = "WVD_EUS_AzureImageBuilder"
 $TemplateUri = "https://raw.githubusercontent.com/TomHickling/WVD-Images/master/1.AzureImageBuilder/DeployAnImage.json"
 
 #Install AZ if not already installed and login
-Install-Module Az -Force
-Connect-AzAccount
+# Install-Module Az -Force
+# Connect-AzAccount
 
-#Regsiter the feature whilst its in preview
-Register-AzProviderFeature -ProviderNamespace Microsoft.VirtualMachineImages -FeatureName VirtualMachineTemplatePreview
+# Regsiter the feature whilst its in preview
+# Register-AzProviderFeature -ProviderNamespace Microsoft.VirtualMachineImages -FeatureName VirtualMachineTemplatePreview
 
 #Check registration is complete - should all say "Registered"
-Get-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages | Select-Object RegistrationState
-Get-AzResourceProvider -ProviderNamespace Microsoft.Storage | Select-Object RegistrationState
+# Get-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages | Select-Object RegistrationState
+# Get-AzResourceProvider -ProviderNamespace Microsoft.Storage | Select-Object RegistrationState
 
 #If anything is not registered run these
-Register-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages
-Register-AzResourceProvider -ProviderNamespace Microsoft.Storage
+# Register-AzResourceProvider -ProviderNamespace Microsoft.VirtualMachineImages
+# Register-AzResourceProvider -ProviderNamespace Microsoft.Storage
 
 #AIB needs rights to Resource Group(s) using its SPN
-#Create a new Resource Group if not already existing
+#Create a new Resource Group if not already existing must be in the US whilst its in preview.
 New-AzResourceGroup -Name $RG -Location 'East US'
 #Provide "contributor" rights to the RG for the Service Principle
 New-AzRoleAssignment -RoleDefinitionName "Contributor" -ApplicationId "cf32a0cc-373c-47c9-9156-0db11f6a6dfc" -ResourceGroupName "$RG"
@@ -31,6 +31,7 @@ New-AzResourceGroupDeployment -ResourceGroupName $RG -TemplateUri $TemplateUri -
 
 #Check Name of Image Template. In the Azure portal select "Show hidden types" in the $RG RG
 $Output.Outputs["imageTemplateName"].Value
+
 #Build the Golden Image
 $ImageTemplateName = $Output.Outputs["imageTemplateName"].Value
 Invoke-AzResourceAction -ResourceGroupName $RG -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ResourceName $ImageTemplateName -Action Run
